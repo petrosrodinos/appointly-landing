1c09416313b1191171c01cdff7663b0161518357
Petros <petros.rodinos@yahoo.com>
2025-10-25 12:15:49 +0300

    axios events

--- features/analytics-events/services/analytics-events.services.ts
+++ features/analytics-events/services/analytics-events.services.ts
@@ -1,23 +1,28 @@
 import { ApiRoutes } from "@/config/api/routes";
 import type { CreateAnalyticsEventDto } from "../interfaces/analytics-events.interfaces";
-import { environments } from "@/config/environments";
+import axiosInstance from "@/config/api/axios";
+// import { environments } from "@/config/environments";
+
 
 export const sendEvent = async (event: CreateAnalyticsEventDto) => {
-    const url = `${environments.API_URL}${ApiRoutes.analytics_events.prefix}`;
-    const payload = JSON.stringify(event);
-    const blob = new Blob([payload], { type: 'application/json' });
+    // const url = `${environments.API_URL}${ApiRoutes.analytics_events.prefix}`;
+    // const payload = JSON.stringify(event);
+    // const blob = new Blob([payload], { type: 'application/json' });
 
     try {
+
+        await axiosInstance.post(ApiRoutes.analytics_events.prefix, event, {
+            headers: { 'Content-Type': 'application/json' },
+        });
+
         // const sent = navigator.sendBeacon(url, blob);
 
-        if (true) {
-            await fetch(url, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: payload,
-                keepalive: true,
-            });
-        }
+        // if (!sent) {
+        //     await axiosInstance.post(ApiRoutes.analytics_events.prefix, event, {
+        //         headers: { 'Content-Type': 'application/json' },
+        //         timeout: 5000,
+        //     });
+        // }
     } catch (err) {
         console.error("Failed to send beacon:", err);
     }
